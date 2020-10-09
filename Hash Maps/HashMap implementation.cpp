8de7a7aaa85b8a716384596e530ac054faa25007
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2020-10-09 19:30:43 +0530

    Update HashMap implementation.cpp

--- Hash Maps/HashMap implementation.cpp
+++ Hash Maps/HashMap implementation.cpp
@@ -14,14 +14,16 @@ class hashMap
         vector< vector<data> > dataMap;
         int hashString(string key);
     public:
-        int get(string key);
-        void set(string key, int value);
+        int getValue(string key);
+        void insertKey(string key, int value);
+        void deleteKey(string key);
+        void modifyValue(string key, int newValue);
+        vector<string> keys();
 
         hashMap(int n){
             dataMap.resize(n);
         }
 };
-
 
 int hashMap::hashString(string key)
 {
@@ -32,8 +34,7 @@ int hashMap::hashString(string key)
     return hash;
 }
 
-
-int hashMap::get(string key)
+int hashMap::getValue(string key)
 {
     int pos = hashString(key);
     if(dataMap[pos].size())
@@ -46,11 +47,33 @@ int hashMap::get(string key)
             }
         }
     }
-    return -1;
+    return 0;
 }
 
-void hashMap::set(string key, int value)
+void hashMap::deleteKey(string key)
 {
+    int pos = hashString(key);
+    if(dataMap[pos].size())
+    {
+        for(int i = 0; i < dataMap[pos].size(); i++)
+        {
+            if(dataMap[pos][i].key == key)
+            {
+                dataMap[pos].erase(dataMap[pos].begin() + i);
+                return;
+            }
+        }
+    }
+    cout<< "Key does not exist...";
+}
+
+void hashMap::insertKey(string key, int value)
+{
+    if(value < 1)
+    {
+        cout<< "Value must be greater than zero";
+        return;
+    }
     int pos = hashString(key);
     data temp;
     temp.key = key;
@@ -58,28 +81,49 @@ void hashMap::set(string key, int value)
     dataMap[pos].push_back(temp);
 }
 
+void hashMap::modifyValue(string key, int newValue)
+{
+    int pos = hashString(key);
+    if(dataMap[pos].size())
+    {
+        for(int i = 0; i < dataMap[pos].size(); i++)
+        {
+            if(dataMap[pos][i].key == key)
+            {
+                dataMap[pos][i].value = newValue;
+                return;
+            }
+        }
+    }
+    cout<< "Key does not exist...";
+}
+
+vector<string> hashMap::keys()
+{
+    vector<string> keys;
+    for(int i = 0; i < dataMap.size(); i++)
+    {
+        if(dataMap[i].size())
+        {
+            for(int j = 0; j < dataMap[i].size(); j++)
+            {
+                keys.push_back(dataMap[i][j].key);
+            }
+        }
+    }
+
+    return keys;
+}
+
 
 int main()
 {
-    hashMap hm(10);
 
-    hm.set("a",1);
-    hm.set("b",2);
-    hm.set("c",3);
-    hm.set("d",4);
-    hm.set("aa",5);
-    hm.set("ba",6);
-    hm.set("ca",7);
-    hm.set("da",8);
-
-    std::cout<<std::endl<<hm.get("a");
-    std::cout<<std::endl<<hm.get("b");
-    std::cout<<std::endl<<hm.get("c");
-    std::cout<<std::endl<<hm.get("d");
-    std::cout<<std::endl<<hm.get("aa");
-    std::cout<<std::endl<<hm.get("ba");
-    std::cout<<std::endl<<hm.get("ca");
-    std::cout<<std::endl<<hm.get("da");
-
-    return 0;
 }
+
+
+/*
+ - delete fucntion
+ - traversal function
+ - key
+ */
